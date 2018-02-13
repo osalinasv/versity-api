3107cd3a627a8c9e0da0c6fbdf658b8c2054f270
Omar Adrian Salinas Villanueva <31166514+omarsalinas16@users.noreply.github.com>
2018-02-12 23:15:05 -0700

    Forgot to add user routes back in

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Mongoose connection
 mongoose.connect('mongodb://localhost/versity');
 mongoose.Promise = global.Promise;
 
@@ -15,7 +20,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api', require('./routes/index'));
+app.use('/api', require('./routes/user'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
